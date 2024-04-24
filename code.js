7ee91e883488ff4801e8ee91b4ae2730a0ef5e78
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-04-24 16:45:47 -0600

    uploading what i have finished so far

--- code.js
+++ code.js
@@ -1,8 +1,47 @@
 function flip(array, n) {
-    return array;
+    flippedArray = new Array(array.length);
+    for (let i = 0; i < array.length; i++) {
+        if (array.length < n)
+            flippedArray[i] = array[array.length - i - 1];
+        if (n > 0)
+            flippedArray[i] = array[n-1];
+        else 
+            flippedArray[i] = array[i];
+        n--;    
+    }
+    return flippedArray;
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    let max = 0;
+    let sorted = 0;
+    
+    for (let i = 0; i <array.length; i++) { //find the largest element
+        if (array[i] > max)
+            max = array[i];
+    }
+    //console.log(max);
+    array = flip(array, array.length); //flip the pancake so that the max is the last element in the array
+    console.log(array);
+    console.log("\n");
+    if (array[array.length - 1] != max) //check that max is the last element
+       array = flip(array, array.length); //if not then flip once more to get it from the first element to the last element
+    console.log(array);
+    console.log("\n");
+    console.log("\n");
+    
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > array[i+1])
+            array = flip(array, i+2);
+        console.log(array);    
+    }
+    console.log("\n");
     return array;
 }
+
+
+let array = [4,5,0,0,0];
+
+console.log(pancakeSort(array));
